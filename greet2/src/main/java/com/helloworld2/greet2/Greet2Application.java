90bba02ea2584acf9fcfe30aeb8822d9c25d1b42
Rio00o <maplesweeet8@gmail.com>
2024-03-25 00:28:12 +0900

    プロジェクトのセットアップ

---
+++ greet2/src/main/java/com/helloworld2/greet2/Greet2Application.java
@@ -0,0 +1,13 @@
+package com.helloworld2.greet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greet2Application.class, args);
+	}
+
+}
---
+++ greet2/src/test/java/com/helloworld2/greet2/Greet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld2.greet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
