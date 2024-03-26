9c9c010f5f4c680a996281175f7d6aed5cf9bee9
Rio00o <maplesweeet8@gmail.com>
2024-03-27 00:38:30 +0900

    /spot でリクエストしたときにspotについてのJSON式を返す API　を実装

---
+++ greet2/src/main/java/com/helloworld2/greet2/Spot.java
@@ -0,0 +1,26 @@
+package com.helloworld2.greet2;
+
+public class Spot {
+    private String name;
+
+    public Spot(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Spot{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
